42c966a6fc6b3302834e5bb9ebe46bf6246a0279
stephaneBalelos <stephanedondyas2@gmail.com>
2024-08-28 16:19:22 +0200

    Content Editor

--- easy-360-app/components/App360/App360PoiEditorComponents/PoiEditorContent.vue
+++ easy-360-app/components/App360/App360PoiEditorComponents/PoiEditorContent.vue
@@ -1,11 +1,30 @@
 <template>
-    <div>
-        Content
+    <div v-if="selectedPOI" class="space-y-2">
+        <UFormGroup label="Name">
+            <template #description>
+            Name of the POI
+            <UIcon name="i-heroicons-information-circle" />
+            </template>
+            <template #default>
+            <UInput v-model="selectedPOI.name" placeholder="Title.." />
+            </template>
+        </UFormGroup>
+        <UFormGroup label="Description">
+            <template #description>
+            Description of the POI
+            <UIcon name="i-heroicons-information-circle" />
+            </template>
+            <template #default>
+            <UTextarea v-model="selectedPOI.description" placeholder="Description" />
+            </template>
+        </UFormGroup>
     </div>
 </template>
 
 <script setup lang="ts">
 
+    const { selectedPOI } = usePOIs();
+    
 </script>
 
 <style scoped>
