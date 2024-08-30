e13290703c98f4ad4a58a0a3846e345bf63a9508
stephaneBalelos <stephanedondyas2@gmail.com>
2024-08-30 15:34:01 +0200

    added tres

--- easy-360-embedded/src/components/Easy360View.ce.vue
+++ easy-360-embedded/src/components/Easy360View.ce.vue
@@ -1,25 +1,48 @@
 <template>
-    <div>
-        {{ props.url }}
-
-        <button @click="increment">increment {{ counter }}</button>
-    </div>
+  <div class="app-360-container">
+    <TresCanvas clear-color="#82DBC5">
+      <TresPerspectiveCamera :position="[9, 9, 9]" />
+      <OrbitControls />
+      <TresMesh :position="[-2, 2, 0]" :rotation="[0, Math.PI, 0]">
+        <TresConeGeometry :args="[1, 1.5, 3]" />
+        <TresMeshToonMaterial color="#82DBC5" />
+      </TresMesh>
+      <TresMesh :position="[0, 0, 0]" cast-shadow>
+        <TresBoxGeometry :args="[1.5, 1.5, 1.5]" />
+        <TresMeshToonMaterial color="#4F4F4F" />
+      </TresMesh>
+      <TresMesh :position="[2, -2, 0]">
+        <TresSphereGeometry />
+        <TresMeshToonMaterial color="#FBB03B" />
+      </TresMesh>
+      <TresDirectionalLight
+        :position="[0, 2, 4]"
+        :intensity="1.2"
+        cast-shadow
+      />
+    </TresCanvas>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { TresCanvas } from '@tresjs/core';
+import { OrbitControls } from '@tresjs/cientos';
+import { onMounted } from 'vue';
 
-    const props = defineProps<{
-        url: string
-    }>()
+const props = defineProps<{
+  pid: string;
+}>();
 
-    const counter = ref(0)
+onMounted(() => {
+  console.log('pid', props.pid);
+});
 
-    const increment = () => {
-        counter.value++
-    }
 </script>
 
 <style scoped>
-
+    .app-360-container {
+        width: 100%;
+        height: 100%;
+        min-height: 500px;
+    }
 </style>
