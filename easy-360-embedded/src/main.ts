0ead452e5ebad199923d2bca8832b39ecf89d07d
stephaneBalelos <stephanedondyas2@gmail.com>
2024-09-12 17:14:02 +0200

    removed embedded vue app

--- easy-360-embedded/src/main.ts
+++
@@ -1,9 +0,0 @@
-import './assets/main.css'
-
-import { defineCustomElement } from 'vue'
-import Easy360View from './components/Easy360View.ce.vue'
-
-const Easy360ViewElement = defineCustomElement(Easy360View)
-
-customElements.define('easy-360', Easy360ViewElement)
-
