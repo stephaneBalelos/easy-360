17ee4c1ac75fee02615ac11464d66ddf0009c50b
stephaneBalelos <stephanedondyas2@gmail.com>
2024-09-17 12:03:28 +0200

    Added Markers

--- easy-360-app/types/app.types.ts
+++ easy-360-app/types/app.types.ts
@@ -2,4 +2,6 @@ import type { Database } from "./database.types";
 
 export type AppProject = Database['public']['Tables']['projects']['Row']
 export type AppScene = Database['public']['Tables']['scenes']['Row']
-export type AppPOI = Database['public']['Tables']['points_of_interest']['Row']
+export type AppPOI = Omit<Database['public']['Tables']['points_of_interest']['Row'], "design_data"> & {
+    design_data: DesignProps
+}
