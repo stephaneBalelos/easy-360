acd31a2795dd71cfb242e412f130fc4466b17c8f
stephaneBalelos <stephanedondyas2@gmail.com>
2024-09-18 11:34:55 +0200

    some route rules

--- easy-360-app/nuxt.config.ts
+++ easy-360-app/nuxt.config.ts
@@ -31,6 +31,9 @@ export default defineNuxtConfig({
     }
   },
   routeRules: {
+    '/': { prerender: true },
+    '/app/**': { ssr: false },
+    '/editor/**': { ssr: false },
     '/api/**': { cors: true },
   }
 })
