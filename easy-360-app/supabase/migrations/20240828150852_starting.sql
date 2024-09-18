5212996b9c0aa9c79c5d95ccc83568c1a2c3b042
stephaneBalelos <stephanedondyas2@gmail.com>
2024-09-19 00:13:58 +0200

    removed on delete cascade on users

--- easy-360-app/supabase/migrations/20240828150852_starting.sql
+++ easy-360-app/supabase/migrations/20240828150852_starting.sql
@@ -1,5 +1,5 @@
 create table public.users (
-    id            uuid references auth.users on delete cascade not null primary key,
+    id            uuid references auth.users not null primary key,
     name          text not null,
     email         text not null unique,
     role          text not null default 'user',
