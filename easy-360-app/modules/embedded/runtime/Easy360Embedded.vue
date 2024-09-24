f62a9026045b36477b660abe8ecf0a55a87e1e7d
stephaneBalelos <stephanedondyas2@gmail.com>
2024-09-24 16:02:44 +0200

    Embedded Sphere Refactoring

--- easy-360-app/modules/embedded/runtime/Easy360Embedded.vue
+++ easy-360-app/modules/embedded/runtime/Easy360Embedded.vue
@@ -10,7 +10,7 @@
       />
       
       <Suspense>
-        <EmbeddedSphere v-if="selectedScene" :url="selectedScene.url" ></EmbeddedSphere>
+        <EmbeddedSphere v-if="selectedScene"></EmbeddedSphere>
       </Suspense>
 
       <TresDirectionalLight
--- easy-360-app/modules/embedded/runtime/components/EmbeddedSphere.vue
+++ easy-360-app/modules/embedded/runtime/components/EmbeddedSphere.vue
@@ -1,63 +1,59 @@
 <template>
-  <primitive ref="sphereRef" :object="sphere" />
+  <!-- <primitive ref="sphereRef" :object="sphere" /> -->
+   <TresMesh ref="sphereRef">
+    <TresSphereGeometry :args="[100, 60, 40]" :scale="[-1, 1, 1]" />
+    <TresShaderMaterial 
+      :fragmentShader="HorizontalBlurShader.fragmentShader"
+      :vertexShader="HorizontalBlurShader.vertexShader" 
+      :uniforms="HorizontalBlurShader.uniforms"
+    />
+   </TresMesh>
 </template>
 
 <script setup lang="ts">
 import { Mesh, MeshBasicMaterial, SphereGeometry, SRGBColorSpace, Texture } from "three";
-import { useTexture, useTresContext, type TresInstance } from "@tresjs/core";
-import { useImage } from "@vueuse/core";
+import { useTexture, useTresContext, useRenderLoop, type TresInstance } from "@tresjs/core";
+
 import { usePreviewState } from "../composables/usePreviewState";
-import { watch, shallowRef } from "vue";
+import { watch, shallowRef, ref, onMounted } from "vue";
+import { HorizontalBlurShader, VerticalBlurShader } from "three/examples/jsm/Addons.js";
 
-type Props = {
-  url: string;
-};
 
-const props = defineProps<Props>();
-const { isLoading, selectedSceneId, scenesTextures } = usePreviewState();
+const { selectedSceneId, scenesTextures } = usePreviewState();
 const sphereRef = shallowRef<TresInstance | null>();
 
-const { isLoading:image_loading } = useImage({ src: props.url });
+const texture = ref<Texture | null>(null);
 
 watch(scenesTextures.value, async () => {
   if (!selectedSceneId.value) return;
   const texture = scenesTextures.value.get(selectedSceneId.value);
   if (!texture) return;
-  const material = await loadTextureMaterial(texture);
-  if (sphereRef.value) {
-    sphereRef.value.material = material;
-  } else {
-    console.error("sphereRef is null");
-  }
+  applyTexture(texture);
 }, { immediate: true, deep: true });
 
 watch(selectedSceneId, async () => {
   if (!selectedSceneId.value) return;
   const texture = scenesTextures.value.get(selectedSceneId.value);
   if (!texture) return;
-  const material = await loadTextureMaterial(texture)
-  if (sphereRef.value) {
-    sphereRef.value.material = material;
-  } else {
-    console.error('sphereRef is null');
-  }
+  applyTexture(texture);
 }, { immediate: true });
 
 
-
-const geometry = new SphereGeometry(100, 60, 40);
-geometry.scale(-1, 1, 1);
-
-const material = new MeshBasicMaterial({ color: 0xffffff, map: null });
-
-const sphere = new Mesh(geometry, material);
+function applyTexture(t: Texture) {
+  if (sphereRef.value) {
+    if (sphereRef.value.material.uniforms && t) {
+      sphereRef.value.material.uniforms.tDiffuse.value = t;
+      sphereRef.value.material.uniforms.h.value = 0 / t.image.width;
+    }
+  } else {
+    console.error('sphereRef is null');
+  }
+}
 
 async function loadTextureMaterial (t: Texture) {
 
-  const material = new MeshBasicMaterial({ map: t });
-  const blurMaterial = new Mesh;
+  texture.value = t;
 
-  return material;
 };
 </script>
 
