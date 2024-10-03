c914cd72fd87265ab297f937fe493dad40bdacb9
stephaneBalelos <stephanedondyas2@gmail.com>
2024-10-03 14:57:20 +0200

    Remove Modules & Lib

--- easy-360-app/modules/embedded/runtime/helpers/index.ts
+++
@@ -1,3 +0,0 @@
-export const mapRange = (num: number, inMin: number, inMax: number, outMin: number, outMax: number) => {
-    return ((num - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
-}
